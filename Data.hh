fe3c2f1a32615cd56c944ba6d7302f003135b948
core-drill <xingchenli2003@outlook.com>
2025-11-25 19:20:44 +0100

    update

--- Data.hh
+++ Data.hh
@@ -7,19 +7,26 @@
 class Data {
  public:
   Data(const std::string& filename);
+  Data(const std::string& file_A, const std::string& file_B);
+  Data(const std::string& file_A, const std::string& file_B, const std::string& file_C, const std::string& file_D);
 
   unsigned int size() const { return m_data.size(); }
   double measurement(int i) const { return m_data[i]; }
   double binCenter(int i) const { return 0; }
-  double binLow(int i) const { return 0; }
-  double binHigh(int i) const { return 0; }
-  double error(int i) const { return 0; }
+  double binLow(int i) const { return m_bins[i]; }
+  double binHigh(int i) const { return m_bins[i+1]; }
+  double error(int i) const { return m_errors[i]; }
+  int checkCompatibility(const Data & in, int n);
+  double chiSquare_ndof();
+  
 
  private:
   Data() {}  // disallow empty data set
   void assertSizes();
   std::vector<double> m_data;
   std::vector<double> m_bins;
+  std::vector<double> m_errors;
 };
+
 
 #endif
--- run.cc
+++ run.cc
@@ -2,6 +2,8 @@
 #include <vector>
 #include <functional>
 #include <string>
+#include <cmath>
+#include <vector>
 
 #include "Data.hh"
 
@@ -51,6 +53,19 @@ bool testCopyConstructor() {
          testEqual("measurement", 10., c.measurement(0));
 }
 
+int testcompatible(double data1, double data2 ,double sigma1, double sigma2) {
+     double diff = data1 - data2;
+     double combined_sigma = std::sqrt(sigma1*sigma1 + sigma2*sigma2);
+     int n = 1;
+     for(int i = 0; i < 5; i += 1) {
+          if (std::abs(diff) <= n * combined_sigma) {
+               break;
+          }
+          n += 1;
+     }
+     return n;
+}
+
 void runTests() {
   std::cout << "running tests...\n";
   std::vector<std::function<bool()> > tests(
@@ -61,19 +76,96 @@ void runTests() {
 }
 
 int main() {
-  using namespace std;
+     using namespace std;
 
-  cout << "******************************************************" << endl;
-  runTests();
-  cout << "******************************************************" << endl;
-  // create an object which holds data of experiment A
-  Data datA("exp_A");
+     cout << "******************************************************" << endl;
+     runTests();
+     cout << "******************************************************" << endl;
+     // create an object which holds data of experiment A
+     Data datA("exp_A");
 
-  // here is the data from experiment A
-  cout << "bin 27: from " << datA.binLow(27) << " to " << datA.binHigh(27)
-       << endl;
-  cout << "measurement of experiment A in bin 27: " << datA.measurement(27)
-       << endl;
+     // here is the data from experiment A
+     cout << "bin 27: from " << datA.binLow(27) << " to " << datA.binHigh(27)
+          << endl;
+     cout << "measurement of experiment A in bin 27: " << datA.measurement(27)
+          << endl;
+     cout << "error of experiment A in bin 27: " << datA.error(27) << endl;
+
+     Data datB("exp_B");
+     cout << "bin 27: from " << datB.binLow(27) << " to " << datB.binHigh(27)
+          << endl;
+     cout << "measurement of experiment B in bin 27: " << datB.measurement(27)
+          << endl; 
+     cout << "error of experiment B in bin 27: " << datB.error(27) << endl; 
+
+     Data datC("exp_C");
+     cout << "bin 27: from " << datC.binLow(27) << " to " << datC.binHigh(27)
+          << endl;
+     cout << "measurement of experiment C in bin 27: " << datC.measurement(27)
+          << endl;
+     cout << "error of experiment C in bin 27: " << datC.error(27) << endl;
+
+     Data datD("exp_D");
+     cout << "bin 27: from " << datD.binLow(27) << " to " << datD.binHigh(27)
+          << endl;
+     cout << "measurement of experiment D in bin 27: " << datD.measurement(27)
+          << endl;
+     cout << "error of experiment D in bin 27: " << datD.error(27) << endl;
+
+     cout << "compatibility of A and B in bin 27: " 
+          << testcompatible(datA.measurement(27), datB.measurement(27),
+                         datA.error(27), datB.error(27)) << " sigma" << endl;
+     
+     cout << "compatibility of A and C in bin 27: " 
+          << testcompatible(datA.measurement(27), datC.measurement(27),
+                         datA.error(27), datC.error(27)) << " sigma" << endl;
+     
+     cout << "compatibility of A and D in bin 27: " 
+          << testcompatible(datA.measurement(27), datD.measurement(27),
+                         datA.error(27), datD.error(27)) << " sigma" << endl;
+     
+     cout << "compatibility of B and C in bin 27: " 
+          << testcompatible(datB.measurement(27), datC.measurement(27),
+                         datB.error(27), datC.error(27)) << " sigma" << endl;
+
+     cout << "compatibility of B and D in bin 27: " 
+          << testcompatible(datB.measurement(27), datD.measurement(27),
+                         datB.error(27), datD.error(27)) << " sigma" << endl;
+     
+     cout << "compatibility of C and D in bin 27: " 
+          << testcompatible(datC.measurement(27), datD.measurement(27),
+                         datC.error(27), datD.error(27)) << " sigma" << endl;
+
+     cout << "number of compatible bins between A and B with 3 sigma: " 
+          << datA.checkCompatibility(datB, 3) << endl;
+     
+     cout << "number of compatible bins between A and C with 3 sigma: " 
+          << datA.checkCompatibility(datC, 3) << endl;
+
+     cout << "number of compatible bins between A and D with 3 sigma: "
+          << datA.checkCompatibility(datD, 3) << endl;
+     
+     cout << "number of compatible bins between B and C with 3 sigma: " 
+          << datB.checkCompatibility(datC, 3) << endl;
+     
+     cout << "number of compatible bins between B and D with 3 sigma: "
+          << datB.checkCompatibility(datD, 3) << endl;
+     
+     cout << "number of compatible bins between C and D with 3 sigma: "
+          << datC.checkCompatibility(datD, 3) << endl;
+
+     Data sum12 = Data("exp_A", "exp_B");
+     cout << "combined measurement of A and B (sum12)in bin 27: " 
+          << sum12.measurement(27) << endl;
+
+     cout << "chi2/ndof of experiment A: " << datA.chiSquare_ndof() << endl;
+     cout << "chi2/ndof of experiment B: " << datB.chiSquare_ndof() << endl;
+     cout << "chi2/ndof of experiment C: " << datC.chiSquare_ndof() << endl;
+     cout << "chi2/ndof of experiment D: " << datD.chiSquare_ndof() << endl;
+
+     Data sum_all = Data("exp_A", "exp_B", "exp_C", "exp_D");
+     cout << "chi2/ndof of combined experiment (A,B,C,D): " 
+          << sum_all.chiSquare_ndof() << endl;
 
   return 0;
 }
